838880447d79dc710d95c2f9e0f7f0d109f43da7
Sarfaraaz <Sarfaraaz.Jahangier@gmail.com>
2019-09-04 13:11:46 +0200

    Fix (mobile) navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,47 @@
 (() => {
+  function executeInQueue(_func, timeout) {
+    if (!timeout) timeout = 10;
 
+    setTimeout(_func, timeout);
+  }
+
+  function bindMenuEvents() {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    if ($navbarBurgers.length > 0) {
+
+      $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+
+        });
+      });
+    }
+  }
+
+  function doTransparentHeaderScrolling() {
+    const mainMenu = document.getElementsByClassName('navbar')[0];
+
+    function shouldStickyNav() {
+      const currentPos = window.scrollY;
+
+      console.log(currentPos);
+
+      if (currentPos > 40) {
+        mainMenu.classList.remove('is-transparent');
+      } else {
+        mainMenu.classList.add('is-transparent');
+      }
+    }
+
+    window.addEventListener('scroll', shouldStickyNav);
+  }
+
+  executeInQueue(bindMenuEvents(), 10);
+  executeInQueue(doTransparentHeaderScrolling(), 20);
 })()
